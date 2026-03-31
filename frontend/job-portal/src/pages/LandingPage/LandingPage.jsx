5652989358afb8f2ca18295e681f3bbb1bd54984
Robera Wakjira <robarobera50@gmail.com>
2026-04-01 00:38:28 +0300

    import analytics component

--- frontend/job-portal/src/pages/LandingPage/LandingPage.jsx
+++ frontend/job-portal/src/pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
+import Analytics from "./components/Analytics";
 
 const LandingPage = () => {
   return (
@@ -8,6 +9,7 @@ const LandingPage = () => {
       <Header />
       <Hero />
       <Features />
+      <Analytics />
     </div>
   );
 };
