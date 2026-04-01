141add905ebdb8445f16b359073f7c7ee4e78543
Robera Wakjira <robarobera50@gmail.com>
2026-04-01 09:44:49 +0300

    import footer

--- frontend/job-portal/src/pages/LandingPage/LandingPage.jsx
+++ frontend/job-portal/src/pages/LandingPage/LandingPage.jsx
@@ -2,14 +2,16 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Analytics from "./components/Analytics";
+import Footer from "./components/Footer";
 
 const LandingPage = () => {
   return (
-    <div className="min-h-screen mb-[100vh]">
+    <div className="min-h-screen">
       <Header />
       <Hero />
       <Features />
       <Analytics />
+      <Footer/>
     </div>
   );
 };
