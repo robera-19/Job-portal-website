716524592207be15c07c1dfb05bc1a8cce46ffaf
Robera Wakjira <robarobera50@gmail.com>
2026-04-06 22:00:15 +0300

    refactor: the header using useAuth

--- frontend/job-portal/src/pages/LandingPage/components/Header.jsx
+++ frontend/job-portal/src/pages/LandingPage/components/Header.jsx
@@ -1,14 +1,12 @@
 import { motion } from "framer-motion";
 import { Briefcase } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../../context/AuthContext";
 
 const Header = () => {
-  const isAuthenticated = true;
-  const user = {
-    name: "Roba",
-    role: "employer",
-  };
+  const { user, isAuthenticated, } = useAuth();
   const navigate = useNavigate();
+  
   return (
     <motion.header
       initial={{ opacity: 0, y: -20 }}
