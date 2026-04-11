07fc3ecb60213162c73e2f843e0e4fb4e88ab77d
Robera Wakjira <robarobera50@gmail.com>
2026-04-11 22:36:29 +0300

    fixed application viewer route issue

--- frontend/job-portal/src/utils/apiPaths.js
+++ frontend/job-portal/src/utils/apiPaths.js
@@ -12,26 +12,26 @@ export const API_PATHS = {
     OVERVIEW: `/api/analytics/overview`,
   },
 
-JOBS: {
-  GET_ALL_JOBS: '/api/jobs',
-  GET_JOB_BY_ID: (id) => `/api/jobs/${id}`,
-  POST_JOB: "/api/jobs",
-  GET_JOBS_EMPLOYER: "/api/jobs/get-jobs-employer",
-  UPDATE_JOB: (id) => `/api/jobs/${id}`,
-  TOGGLE_CLOSE: (id) => `/api/jobs/${id}/toggle-close`,
-  DELETE_JOB: (id) => `/api/jobs/${id}`,
-  SAVE_JOB: (id) => `/api/save-jobs/${id}`,
-  UNSAVE_JOB: (id) => `/api/save-jobs/${id}`,
-  GET_SAVED_JOBS: 'api/save-jobs/my',
-},
+  JOBS: {
+    GET_ALL_JOBS: "/api/jobs",
+    GET_JOB_BY_ID: (id) => `/api/jobs/${id}`,
+    POST_JOB: "/api/jobs",
+    GET_JOBS_EMPLOYER: "/api/jobs/get-jobs-employer",
+    UPDATE_JOB: (id) => `/api/jobs/${id}`,
+    TOGGLE_CLOSE: (id) => `/api/jobs/${id}/toggle-close`,
+    DELETE_JOB: (id) => `/api/jobs/${id}`,
+    SAVE_JOB: (id) => `/api/save-jobs/${id}`,
+    UNSAVE_JOB: (id) => `/api/save-jobs/${id}`,
+    GET_SAVED_JOBS: "api/save-jobs/my",
+  },
 
-APPLICATIONS: {
+  APPLICATIONS: {
     APPLY_TO_JOB: (id) => `/api/applications/${id}`,
     GET_ALL_APPLICATIONS: (id) => `/api/applications/job/${id}`,
-    UPDATE_STATUS: (id) => `api/applications/${id}/status`,
-},
+    UPDATE_STATUS: (id) => `/api/applications/${id}/status`,
+  },
 
-IMAGE: {
+  IMAGE: {
     UPLOAD_IMAGE: "/api/auth/upload-image", // Upload profile picture
-},
+  },
 };
