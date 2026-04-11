7e885150a963f36c957ff70ed42824a8e32f3be9
Robera Wakjira <robarobera50@gmail.com>
2026-04-11 22:37:27 +0300

    export getinitials

--- frontend/job-portal/src/utils/helper.js
+++ frontend/job-portal/src/utils/helper.js
@@ -37,3 +37,12 @@ export const validateAvatar = (file) => {
 
   return "";
 };
+
+export const getInitials = (name) => {
+  return name
+    .split(" ")
+    .map((word) => word.charAt(0))
+    .join("")
+    .toUpperCase()
+    .slice(0, 2);
+};
